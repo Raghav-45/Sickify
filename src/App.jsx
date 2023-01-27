3c46c9c21e0ca8e4093c856c155f096a3593ac09
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-01-27 20:35:33 +0530

    Full height

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='h-full bg-green-500'>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
