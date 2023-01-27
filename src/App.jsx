0f661d73354586eca24066a78eaf10e334309e40
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-01-27 21:29:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <footer
+      className="bg-green-700
+             text-3xl text-white text-center
+             border-t-4 border-red-500
+             fixed
+             inset-x-0
+             bottom-0
+             p-4">
+      This is sticky fixed Footer.
+    </footer>
     </div>
   )
 }
