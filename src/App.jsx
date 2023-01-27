4ffefe79d4110d12c4cda3bf3093330ece05d0fe
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-01-27 21:37:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 function App() {
   return (
-    <div className='h-[100vh] bg-green-500'>
-      <h1 className="text-3xl font-bold underline">
+    <div className='h-[100vh] bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=320&h=320&q=80")]'>
+      <h1 className='text-3xl font-bold underline'>
         Hello world!
       </h1>
-      <footer
-      className="bg-green-700
-             text-3xl text-white text-center
-             border-t-4 border-red-500
-             fixed
-             inset-x-0
-             bottom-0
-             p-4">
-      This is sticky fixed Footer.
-    </footer>
+      <footer className='bg-black text-xl text-white backdrop-blur-md text-center fixed inset-x-0 bottom-0'>
+        This is sticky fixed Footer.
+      </footer>
     </div>
   )
 }
