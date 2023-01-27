c9addee688f0b4fc91f235f67396f19d29562830
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-01-27 20:36:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className='h-full bg-green-500'>
+    <div className='h-screen bg-green-500'>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
