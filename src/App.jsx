95eb3398306301a4ca95e473cbac85ab8600a6b1
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-01-27 22:24:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import { FiSearch } from 'react-icons/fi'
 import { BiLibrary } from 'react-icons/bi'
 
 function App() {
+
+  const timeGreet = () => {
+    const hour = Date.now().hour
+    if (hour < 12) {
+      return 'Morning'
+    }
+    if (hour < 17) {
+      return 'Afternoon'
+    }
+    return 'Evening'
+  }
+
   return (
     <div className='h-[100vh] bg-center bg-[url("https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=320&h=320&q=80")]'>
-      <h1 className='text-3xl font-bold underline'>
-        Hello world!
+      <h1 className='text-3xl font-bold'>
+        Good {timeGreet()}!
       </h1>
-      <footer className='bg-gradient-to-t bg-black/10 text-white backdrop-blur-lg text-center fixed inset-x-0 bottom-0'>
-        <div class="grid grid-cols-3 gap-0 m-5 text-2xl">
+      <footer className='bg-gradient-to-t from-black backdrop-blur-lg text-white text-center fixed inset-x-0 bottom-0'>
+        <div class="grid grid-cols-3 gap-0 m-5 text-xl">
           <div className='mx-auto'><AiFillHome/></div>
           <div className='mx-auto'><FiSearch/></div>
           <div className='mx-auto'><BiLibrary/></div>
