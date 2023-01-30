26f9d1413109b37c008ea167091b4cc876ccadcc
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-01-30 19:26:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
         </div>
         <AvatarIcon/>
       </div>
-      <MusicCard Name='raghav' Artist='Bro' Image='https://lh3.googleusercontent.com/a/AEdFTp7pvJ8QlLi9q9I_lHMylrsWyDuaZODMG6i2hKdH=s83-c-mo' />
+      <MusicCard Name='raghav' Artist='Bro' Image='https://lh3.googleusercontent.com/X2LsGDinExErsug_2nybr0YUqOXPk0aVREvGlZOoMiRIsM4pR-WMqV_rOFIEDfVEMbXTwxjCSJuUNy8=w544-h544-l90-rj' />
       <footer className='bg-gradient-to-t from-dark backdrop-blur-lg text-white text-center fixed inset-x-0 bottom-0'>
         <div className='grid grid-cols-3 gap-0 m-5 text-xl'>
           <div className='mx-auto'><AiFillHome/></div>
