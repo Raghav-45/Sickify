ba128c8a27a47574255d70db8fa0b8dfbfcdf956
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-01-30 20:06:25 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark': '#0F0C0C',
+        'dark': '#0C0C0F',
         'Darkest': '#191414',
         'light': '#282828',
         'lightest': '#B3B3B3',
