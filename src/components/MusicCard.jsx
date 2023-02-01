ef289926de0a5e2ce589b64b9444b943f03dc1fe
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-02-01 16:21:55 +0530

    Added Player, YT, Footer Info

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -11,9 +11,9 @@ export const MusicCard = ((props) => {
       <div className="bg-light w-full h-auto p-4 rounded-xl shadow-md">
         <div className='relative aspect-square w-full shadow-[0_8px_24px_rgb(0,0,0,50%)] mb-3 overflow-hidden rounded-lg'>
           <div className='absolute playButton bg-green-500 rounded-full h-10 w-10 m-2 flex right-0 bottom-0 items-center justify-center transition opacity-0 group-hover:opacity-100 translate-y-7 group-hover:-translate-y-0'>
-            <button onClick={props.onPlayButton} ><FaPlay className='text-white text-1xl'/></button>
+            <button onClick={props.onPlayButton}><FaPlay className='text-white text-1xl'/></button>
           </div>
-          <img src={Image} className='h-full w-full' />
+          <img src={Image} className='h-full w-full'/>
         </div>
         <h1 className='text-sm text-white font-semibold tracking wide whitespace-nowrap overflow-x-hidden text-ellipsis'>{Name}</h1>
         <h2 className='text-xs text-lightest tracking-wide whitespace-nowrap overflow-x-hidden text-ellipsis pb-0'>{Artist}</h2>
