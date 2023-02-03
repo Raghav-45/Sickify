266242cedc0839ce5498c493f84d2d6a4dd75eae
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-02-03 20:12:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ export default function App() {
   const greetByTime = () => {
     const d = new Date()
     const hour = d.getHours()
-    if (hour < 12) { return 'Morning' }
+    if (hour < 12) { return 'morning' }
     if (hour < 18) { return 'afternoon' }
-    return 'Evening'
+    return 'evening'
   }
 
   return (<>
