fee766580ecfd47719dabdc2515ffe8e7f25c724
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-02-03 20:51:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ContextProvider>
       <Layout>
-        <SearchPage />
+        <App />
       </Layout>
     </ContextProvider>
   </React.StrictMode>,
